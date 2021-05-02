5a262cf13d1bc0446d8d169497d9839c803e09ed
Simon BERNARD <simon44360@gmail.com>
2021-05-02 09:07:33 +0200

    Initial commit with Spring Initializr

---
+++ src/test/java/eu/nickel/guildespringbootexample/GuildeSpringbootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.nickel.guildespringbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuildeSpringbootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
